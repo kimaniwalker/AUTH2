115e370d38d41d2f54cc27a687a22f8a1ad656cf
Kimani Walker <kimaniwalker@gmail.com>
2018-07-20 17:16:27 -0500

    Fixed .then & added config to mail.js

--- server/src/routes/contactform.js
+++ server/src/routes/contactform.js
@@ -2,17 +2,19 @@ import { Router } from 'express';
 import { sendEmail } from '../utils/mail';
 let router = Router();
 
-router.post('/', (req,res,next) => {
-    let messageBody = `Name: ${req.body.name}
+router.post('/', (req, res, next) => {
+    let messageBody =
+        `Name: ${req.body.name}
     Email: ${req.body.email}
     Message: ${req.body.message}`;
 
-    sendEmail('kimaniwalker@gmail.com', 'no-reply-keyswebdesign@gmail.com','New Contact Form Submission', messageBody)
-    .then((res) => {
-        res.sendStatus(201);
-    }).catch((err) => {
-        next(err);
-    });
+    sendEmail('kimaniwalker@gmail.com', 'no-reply-keyswebdesign@gmail.com', 'New Contact Form Submission', messageBody)
+        .then((response) => {
+            console.log(response);
+            res.sendStatus(201);
+        }).catch((err) => {
+            next('this is an error' + err);
+        });
 })
 
 export default router;
--- server/src/utils/mail.js
+++ server/src/utils/mail.js
@@ -1,5 +1,10 @@
 import mailgunLoader from 'mailgun-js';
-let mailgun = mailgunLoader({ apiKey:process.env.MAILGUN_API_KEY, domain: 'sandboxbc2a8220421648b3bc7044e84d51c5fb.mailgun.org' });
+import { config } from '../config';
+let mailgun = mailgunLoader({ apiKey: config.MAILGUN_API_KEY, domain:'sandboxbc2a8220421648b3bc7044e84d51c5fb.mailgun.org'});
+
+
+
+
 
 function sendEmail(to, from, subject, content) {
     let data = {
